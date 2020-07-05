b9504a1ccbfde4c2c42af5f2783944e836766912
ekremkose55 <67019372+ekremkose55@users.noreply.github.com>
2020-07-05 14:09:44 -0500

    Update ClassOne.java
    yeni degisiklik

--- src/git_proje04/ClassOne.java
+++ src/git_proje04/ClassOne.java
@@ -3,8 +3,8 @@ package git_proje04;
 public class ClassOne {
 
 	public static void main(String[] args) {
-		System.out.println("Ilk commit local masterdan");
-
+		System.out.println("Ilk commit local masterdan");		
+                System.out.println("Ilk commit local masterdan");
 	}
 
 }
